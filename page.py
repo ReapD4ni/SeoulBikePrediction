2cead6a490f82d7b33b887c6fe9f28bc0cc37dd7
ReapD4ni <115945147+ReapD4ni@users.noreply.github.com>
2023-11-19 11:16:52 +0100

    Update page.py

--- page.py
+++ page.py
@@ -15,7 +15,7 @@ page = st.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
 
 
 def show_predict_page():
-    st.title("Seoul Bike Data Prdeiction :bicycle:")
+    st.title("Seoul Bike Data Prdeiction 🚲")
 
     st.write("### We need some information to predict rented bike count")
 
